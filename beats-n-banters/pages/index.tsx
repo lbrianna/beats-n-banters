6b644556acc90bff7e916bef7fe1afe790fc8d15
Raquel <raquelgr@bu.edu>
2023-10-23 19:23:04 -0400

    added comments

--- beats-n-banters/pages/index.tsx
+++ beats-n-banters/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Inter } from "next/font/google";
 import React, { useEffect, useState } from "react";
+import 'dotenv/config'
 
 const inter = Inter({ subsets: ["latin"] });
 
-const Home: React.FC = () => {
+function App () {
   const [text, setText] = useState("");
-  //Fetching Techy API
+  //Fetching Techy API 
   const fetchData = async () => {
     try {
       await fetch("http://localhost:3000/api/hello", {
@@ -24,34 +25,32 @@ const Home: React.FC = () => {
       console.log(error);
     }
   };
-  useEffect(() => {
-    fetchData();
-  }, []);
   //Fetching Spotify API
-  const CLIENT_ID = process.env.API_KEY;
-  const CLIENT_SECRET = process.env.API_SECRET;
-  function App() {
-    useEffect(() => {
-      //API Access Token
-      const [accessToken, setaccesstoken] = useState("");
-      var authParameters = {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body:
-          "grant_type=client_credentials&client_id=" +
-          CLIENT_ID +
-          "&client_secret=" +
-          CLIENT_SECRET,
-      };
-      fetch("https://accounts.spotify.com/api/token", authParameters)
-        .then((result) => result.json())
-        .then((data) => setaccesstoken(data.access_token));
-    }, []);
-  }
+  const CLIENT_ID = process.env.NEXT_PUBLIC_API_KEY;
+  const CLIENT_SECRET = process.env.NEXT_PUBLIC_API_SECRET;
+
+  useEffect(() => {
+    //API Access Token
+    var authParameters = {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
+
+    console.log(authParameters);
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+  }, []);
+  
   //We need to: 1-Define a search function to look up every song name on spotify
   // 2- After song is found: add it to the playlist (additional helper function)
   async function search(word: string) {}
-
+  //Main
   return (
     <>
       <p></p>
@@ -59,4 +58,4 @@ const Home: React.FC = () => {
     </>
   );
 };
-export default Home;
+export default App;
