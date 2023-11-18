264cb6db9e82ca547c4b99ca8bffb4a2d596a8e8
Raquel <raquelgr@bu.edu>
2023-11-18 11:59:01 -0500

    Database settings

--- server/server.js
+++ server/server.js
@@ -3,13 +3,22 @@ const app = express();
 import querystring from "query-string";
 import request from "request";
 import cors from "cors";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import mongoose from "mongoose";
 
 const PORT = 5001;
 var client_id = "22587a51ddb44d45ac62822b6013d144";
 var client_secret = "fcbdd00791b84d08a2acc6ff84e6513e";
 var redirect_uri = "http://localhost:5001/callback";
+
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://raquelgr:3hz5SZ7xZ8AQ9WJI@cluster0.qxk9qv0.mongodb.net/?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log("Successful");
+}
 
 app.use(cors());
 
@@ -28,6 +37,13 @@ app.get("/login", function (req, res, next) {
       })
   );
 });
+//Schema
+const sch = {
+  token: String,
+  refresh: String,
+  username: String,
+};
+const monmodel = mongoose.model("USER", sch);
 
 app.get("/callback", async function (req, res, next) {
   var code = req.query.code || null;
@@ -77,13 +93,29 @@ app.get("/callback", async function (req, res, next) {
         .then((data) => {
           userdata = data;
         });
-
-      // let merged = {
-      //   ...body,
-      //   ...userdata,
-      //   ...{ url: "http://localhost:3000" },
-      // };
-      // res.send(merged);
+      //Look for username
+      if (await monmodel.findOne({ username: userdata.id }).exec()) {
+        let user = userdata.id;
+        let newToken = body.access_token;
+        let newRefresh = body.refresh_token;
+        request.post("/put", async (req, res) => {
+          const update = await monmodel.updateOne(
+            { username: user },
+            { token: newToken, refresh: newRefresh },
+            { new: true }
+          );
+        });
+      } else {
+        //Post request to DATABASE
+        request.post("/post", async (req, res) => {
+          const usertoken = new monmodel({
+            token: body.access_token,
+            refresh: body.refresh_token,
+            username: userdata.id,
+          });
+          const val = await usertoken.save();
+        });
+      }
       res.redirect("http://localhost:3000/redirect");
     } else {
       res.redirect(
